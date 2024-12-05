6b8ffd8cdf2b250489695942d816119daae08cce
alineccorrea <alineccorrea97@gmail.com>
2024-12-05 20:56:39 -0300

    Correção de variável de acesso ao banco

--- db.ts
+++ db.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const dbName = process.env.DB_NAME!;
 const dbUser = process.env.DB_USER!;
-const dbPass = process.env.DB_PASSWORD;
+const dbPass = process.env.DB_PASS;
 const dbHost = process.env.DB_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
